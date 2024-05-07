c23b8ffb485448c9063f015b193852263b0495cd
Alex Kozak <56194044+Alex-byte256@users.noreply.github.com>
2024-05-07 12:58:05 +0300

    arrow icon

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,6 +1,10 @@
 import css from "./Sidebar.module.css"
+import sideBarArrow from '../../icons/sideBarArrow.png'
 export const Sidebar = () =>{
     return(
-        <div className={css.sideBarWrap}>SideBar test</div>
+        <div className={css.sideBarWrap}>
+            SideBar test
+            <img src={sideBarArrow} width={40} alt="sideBarArrow" className={css.arrowIcon}/>
+        </div>
     )
 }
